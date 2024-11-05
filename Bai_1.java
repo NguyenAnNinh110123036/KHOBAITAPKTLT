a1b43d336d70da2e61f1c1ae3bcd588a0e9019ab
NguyenAnNinh110123036 <147956152+NguyenAnNinh110123036@users.noreply.github.com>
2024-11-05 08:40:12 +0700

    Add files via upload

---
+++ Bai_1.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+public class Bai_1 {
+
+	public static void main(String[] args) {
+		Scanner sc =  new Scanner(System. in);
+		System.out.println("Moi ban nhap a: ");
+		int a = sc.nextInt();
+		System.out.println("Moi ban nhap b: ");
+		int b = sc.nextInt();
+		if(a>b && a == b)
+		{
+			System.out.print(a);
+		}
+		else
+		{
+			System.out.print(b);
+		}
+	}
+
+}
